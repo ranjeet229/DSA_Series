fa24225797da69980de08b34fed8f8ed52804b7d
Ranjeet <kiranjeetkr80@gmail.com>
2025-03-30 16:07:35 +0530

    bst_Problem

--- Binary_Tree/problembst.cpp
+++ Binary_Tree/problembst.cpp
@@ -3,5 +3,41 @@
 #include<iostream>
 using namespace std;
 
-//Ques 1:- bst in 
-//ques 2....
+
+struct TreeNode {
+    int val;
+     TreeNode *left;
+     TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ };
+
+//Ques 1:- Maximum height of a binary tree
+
+int maxDepth(TreeNode* root){
+    if(root==NULL){
+        return 0;
+    }
+    int l=maxDepth(root->left);
+    int r=maxDepth(root->right);
+    return max(l,r)+1;
+}
+
+//ques 2:- Check for Balanced binary tree.....>>>>
+
+bool isBalanced(TreeNode* root){
+    return check(root) != -1;
+}
+int check(TreeNode* root){
+    if(root==NULL) return 0;
+
+    int l=check(root->left);
+    if(l==-1) return -1;
+
+    int r=check(root->right);
+    if(r==-1) return -1;
+
+    if(abs(l-r)>1) return -1;
+    return max(l,r)+1;
+}
