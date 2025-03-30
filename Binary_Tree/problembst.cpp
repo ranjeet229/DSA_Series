00b71f3922ad84843bfae600b0e446c92f99f9eb
Ranjeet <kiranjeetkr80@gmail.com>
2025-03-30 18:22:32 +0530

    maxpath

--- Binary_Tree/problembst.cpp
+++ Binary_Tree/problembst.cpp
@@ -41,3 +41,41 @@ int check(TreeNode* root){
     if(abs(l-r)>1) return -1;
     return max(l,r)+1;
 }
+
+//ques 3:- diameter of a binary tree.....>>>>>
+
+int diameterbst(TreeNode* root){
+    int diameter=0;
+    findMax(root, diameter);
+    return diameter;
+}
+int findMax(TreeNode* root, int &diameter){
+    if(root==NULL) return 0;
+
+    int l=findMax(root->left, diameter);
+    int r=findMax(root->right, diameter);
+    diameter=max(diameter, l+r);
+
+    return 1+max(l,r);
+}
+
+//ques 4:-Maximum Path sum in binary tree....>>>>>
+
+int maxPathSum(TreeNode* root){
+    int maxi=0;
+    maxPath(root, maxi);
+    return maxi;
+}
+int maxPath(TreeNode* root, int &maxi){
+    if(root==NULL) return 0;
+
+    int l=max(0, maxPath(root->left, maxi));
+    int r=max(0,maxPath(root->right, maxi));
+
+    maxi=max(maxi, l+r+root->val);
+    return max(l,r)+root->val;
+}
+
+//ques 5:-Check if two trees are indentical or not in bst .....>>>>>
+
+
