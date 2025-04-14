4bd9cf68d0c953b1b31810b1637caa49557fade9
Ranjeet <kiranjeetkr80@gmail.com>
2025-04-14 18:46:12 +0530

    bst_2

--- BST/binarysearchtre.cpp
+++ BST/binarysearchtre.cpp
@@ -65,4 +65,62 @@ int floorBst(TreeNode* root, int key){
     }
     return floor;
 }
-//Insert a given node in bst ...........>>>
+// Ques 4:- Insert a given node in bst ...........>>>
+TreeNode* insertbst(TreeNode* root, int val){
+    if(root==NULL) return new TreeNode(val);
+    TreeNode* curr=root;
+    while(true){
+        if(curr->val <=val){
+            if(curr->right !=NULL){
+                curr=curr->right;
+            }else{
+                curr->right = new TreeNode(val);
+                break;
+            }
+        }else{
+            if(curr->left !=NULL) {
+                curr=curr->left;
+            }else{
+                curr->left=new TreeNode(val);
+                break;
+            }
+        }
+    }
+    return root;
+}
+
+//Ques5:-  Delete a node in a bst........>>>>>
+
+TreeNode* deleteNode(TreeNode* root, int key) {
+    if (!root) return nullptr;
+
+    if (key < root->val) {
+        root->left = deleteNode(root->left, key);
+    } else if (key > root->val) {
+        root->right = deleteNode(root->right, key);
+    } else {
+        if (!root->left) {
+            TreeNode* temp = root->right;
+            delete root;
+            return temp;
+        } else if (!root->right) {
+            TreeNode* temp = root->left;
+            delete root;
+            return temp;
+        } else {
+
+            TreeNode* successor = minValueNode(root->right);
+            root->val = successor->val;
+            root->right = deleteNode(root->right, successor->val);
+        }
+    }
+    return root;
+}
+
+TreeNode* minValueNode(TreeNode* node) {
+    while (node && node->left)
+        node = node->left;
+    return node;
+}
+
+//Ques 6:- 
