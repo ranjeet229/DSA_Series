9ec94ca6d26b41e199d478edad50844509603607
Ranjeet <kiranjeetkr80@gmail.com>
2025-07-28 23:51:05 +0530

    topo

--- Revisison/array_revision.cpp
+++ Revisison/array_revision.cpp
@@ -111,22 +111,52 @@ using namespace std;
 // }
 
 //using two pointer
-int removeDuplicates(int arr[], int n ){
-    int i=0;
-    for(int j =1;j<n;j++){
-        if(arr[i] !=arr[j]){
-            i++;
-            arr[i]=arr[j];
-        }
+// int removeDuplicates(int arr[], int n ){
+//     int i=0;
+//     for(int j =1;j<n;j++){
+//         if(arr[i] !=arr[j]){
+//             i++;
+//             arr[i]=arr[j];
+//         }
+//     }
+//     return i+1;
+// }
+// int main() {
+//   int arr[] = {1,1,2,2,2,3,3};
+//   int n = sizeof(arr)/sizeof(arr[0]);
+//   int k = removeDuplicates(arr, n);
+//   cout << "The array after removing duplicate elements is " << endl;
+//   for (int i = 0; i < k; i++) {
+//     cout << arr[i] << " ";
+//   }
+// }
+
+//Ques 5: - Left Rotate the array by one ...>>>
+
+// void solve(int arr[], int n){
+//     int temp[n];
+//     for(int i=0;i<n;i++){
+//         temp[i-1]=arr[i];
+//     }
+//     temp[n-1] = arr[0];
+//     for(int i=0;i<n;i++){
+//         cout<<temp[i]<<" ";
+//     }
+//     cout<<endl;
+// }
+void solve(int arr[], int n ){
+    int temp=arr[0];
+    for(int i=0;i<n-1;i++){
+        arr[i]=arr[i+1];
     }
-    return i+1;
+    arr[n-1] = temp;
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<" ";
+    }
 }
-int main() {
-  int arr[] = {1,1,2,2,2,3,3};
-  int n = sizeof(arr)/sizeof(arr[0]);
-  int k = removeDuplicates(arr, n);
-  cout << "The array after removing duplicate elements is " << endl;
-  for (int i = 0; i < k; i++) {
-    cout << arr[i] << " ";
-  }
+int main(){
+    int n=5;
+    int arr[] = {1,2,3,4,5};
+    solve(arr, n);
+
 }
