51b69825ba2025216e9a26c004ad52d079702704
Ranjeet <kiranjeetkr80@gmail.com>
2025-07-29 21:48:50 +0530

    kahn's algo

--- Graphh/Topologicalsort.cpp
+++ Graphh/Topologicalsort.cpp
@@ -29,5 +29,32 @@ vector<int> toposort(int v, vector<int> adj[]){
     return ans;
 }
 
-// Ques 2: -
+// Ques 2: - Topo sort using bfs / kahn's algorithm ....>>>
+
+vector<int> toposort(int v, vector<int> adj[]){
+    int indegree[v] ={0};
+    for(int i=0;i<v;i++){
+        for(auto it : adj[i]){
+            indegree[it]++;
+        }
+    }
+    queue<int> q;
+    for(int i=0;i<v;i++){
+        if(indegree[i] ==0){
+            q.push(i);
+        }
+    }
+    vector<int> topo;
+    while(!q.empty()){
+        int node = q.front();
+        q.pop();
+        topo.push_back(node);
+
+        for(auto it : adj[node]){
+            indegree[it]--;
+            if(indegree[it] == 0) q.push(it);
+        }
+    }
+    return topo;
+}
 
