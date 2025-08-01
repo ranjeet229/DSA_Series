3c798d4f0bfee15a69078cca87f494352f153d61
Ranjeet <kiranjeetkr80@gmail.com>
2025-08-02 02:08:49 +0530

    order

--- Graphh/Topologicalsort.cpp
+++ Graphh/Topologicalsort.cpp
@@ -141,4 +141,41 @@ bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
         if(topo.size() == N) return true;
         return false;
     }
-//course II:- 
+//course II:- return the order value and  pair order is reversed in this case 
+
+ vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
+        int N= numCourses;
+        vector<vector<int>> adj(N);
+        for(auto& it : prerequisites){
+            int u=it[1];
+            int v=it[0];
+            adj[u].push_back(v);
+        }
+        vector<int>indegree(N,0);
+        for(int i=0;i<N;i++){
+            for(auto& it : adj[i]){
+                indegree[it]++;
+            }
+        }
+        queue<int>q;
+        for(int i=0;i<N;i++){
+            if(indegree[i] == 0){
+                q.push(i);
+            }
+        }
+        vector<int> topo;
+        while(!q.empty()){
+            int node = q.front();
+            q.pop();
+            topo.push_back(node);
+
+            for(auto it : adj[node]){
+                indegree[it]--;
+
+                if(indegree[it] == 0) q.push(it); 
+            }
+        }
+        if(topo.size() == N) return topo;
+        return {};
+    }
+
